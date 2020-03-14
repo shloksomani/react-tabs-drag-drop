1670f23c0a1f11beccc2c353288ca20e1184adbd
shloksomani <shloksomani@ymail.com>
2020-03-14 16:17:16 -0400

    Updates

---
+++ precache-manifest.862c085b5a5ef21d23ccd7fbec52d3a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "616df5472456a673d73065af4c9769e8",
+    "url": "/react-tabs-drag-drop/index.html"
+  },
+  {
+    "revision": "ec68edbd289c725a6dc5",
+    "url": "/react-tabs-drag-drop/static/css/main.30cda563.chunk.css"
+  },
+  {
+    "revision": "9a6c49eec3bf7b4d5d1b",
+    "url": "/react-tabs-drag-drop/static/js/2.ce04f4b7.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/react-tabs-drag-drop/static/js/2.ce04f4b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec68edbd289c725a6dc5",
+    "url": "/react-tabs-drag-drop/static/js/main.712905ec.chunk.js"
+  },
+  {
+    "revision": "8536760ebb1ab3173869",
+    "url": "/react-tabs-drag-drop/static/js/runtime-main.a255fc7b.js"
+  }
+]);
